e27d9d8f75a4c03615a04291edb6b3f82651f29a
Faruk GÜNGÖR <farcompen71@gmail.com>
2018-02-05 14:38:36 +0300

    Add files via upload

---
+++ FortigateUrlImporter/Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace FortigateUrlImporter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox3 = new System.Windows.Forms.RichTextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.richTextBox1.Location = new System.Drawing.Point(12, 57);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(328, 568);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(25, 28);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Veri Al ";
+            this.button1.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.richTextBox2.Location = new System.Drawing.Point(451, 57);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(328, 568);
+            this.richTextBox2.TabIndex = 2;
+            this.richTextBox2.Text = "";
+            // 
+            // richTextBox3
+            // 
+            this.richTextBox3.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.richTextBox3.Location = new System.Drawing.Point(824, 57);
+            this.richTextBox3.Name = "richTextBox3";
+            this.richTextBox3.Size = new System.Drawing.Size(328, 568);
+            this.richTextBox3.TabIndex = 3;
+            this.richTextBox3.Text = "";
+            // 
+            // button2
+            // 
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button2.Location = new System.Drawing.Point(359, 194);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 41);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Script Oluştur >>";
+            this.button2.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Visible = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button3.Location = new System.Drawing.Point(359, 276);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 40);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Adres Group >>";
+            this.button3.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Visible = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(792, 636);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(70, 17);
+            this.radioButton1.TabIndex = 6;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Url Listesi";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.Visible = false;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(792, 659);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(89, 17);
+            this.radioButton2.TabIndex = 7;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Adress Group";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.Visible = false;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(898, 653);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(103, 23);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "Scripti Kaydet";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Visible = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1250, 726);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.richTextBox3);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "Fortigate URL Importer";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.RichTextBox richTextBox3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ FortigateUrlImporter/Form1.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FortigateUrlImporter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        public   string dosyaYolu { get; set; }
+        public   string dosyaAdi { get; set; } 
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DosyaSec();
+            txt_dosya_getir();
+            button2.Visible = true;
+        }
+     
+        
+        //Creating fqdn script for fortigate 
+        public void script_olustur()
+
+        {
+            try
+            {
+                richTextBox2.Text = " config firewall address \n               edit ";
+                StreamReader SR = new StreamReader(dosyaYolu);
+                string yazi = SR.ReadLine();
+                while (yazi != null)
+                {
+                    richTextBox2.Text = richTextBox2.Text + '"' + yazi + '"' + "\n    " + "                          set type fqdn   \n                              set fqdn " + '"' + yazi + '"' + " \n               next      \n               edit ";
+                    yazi = SR.ReadLine();
+                }
+
+                SR.Close();
+                richTextBox2.Text = richTextBox2.Text + "\n               end \n                 \n ";
+            }
+            catch (Exception eror)
+            {
+                throw eror;
+            }
+
+        }
+
+        //reading data from txt file then fill the richtextbox1 
+        public void txt_dosya_getir()
+        {
+            try
+            {
+                richTextBox1.Text = " ";
+                StreamReader SR = new StreamReader(dosyaYolu);
+                string yazi = SR.ReadLine();
+                while (yazi != null)
+                {
+                    richTextBox1.Text = richTextBox1.Text + yazi + "\n";
+                    yazi = SR.ReadLine();
+                }
+
+                SR.Close();
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show(error.ToString());
+            }
+
+        }
+
+        //Save script output as txt file 
+        public void kaydet()
+        {
+            SaveFileDialog save = new SaveFileDialog();
+            save.Filter = "Metin Dosyası|*.txt";
+            save.OverwritePrompt = true;
+            save.CreatePrompt = true;
+            if (save.ShowDialog() == DialogResult.OK)
+            {
+                StreamWriter yazici = new StreamWriter(save.FileName);
+                if (radioButton1.Checked == true)
+                    yazici.WriteLine(richTextBox2.Text);
+                else if (radioButton2.Checked == true)
+                    yazici.WriteLine(richTextBox3.Text);
+                yazici.Flush();
+
+                yazici.Close();
+                MessageBox.Show("Başarılı şekilde kaydedildi  ");
+            }
+
+        }
+        // Adress Group creating for fortigate 
+        public void AdresGroupCreate()
+        {
+            try
+            {
+
+                string groupName = "PhishingUrl";
+                richTextBox3.Text = " config firewall addrgrp \n      edit " + '"' + groupName + '"' + "\n          set member ";
+                FileStream fs = new FileStream(dosyaYolu, FileMode.Open, FileAccess.Read);
+                StreamReader SR = new StreamReader(fs);
+                string yazi = SR.ReadLine();
+                while (yazi != null)
+                {
+                    richTextBox3.Text = richTextBox3.Text + " " + '"' + yazi + '"' + " ";
+                    yazi = SR.ReadLine();
+                }
+                fs.Close();
+                SR.Close();
+                richTextBox3.Text = richTextBox3.Text + "\n      next\n      end";
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show(error.ToString());
+            }
+
+        }
+
+        //choose url list file (txt) 
+        public void DosyaSec()
+        {
+            OpenFileDialog file = new OpenFileDialog();
+            file.Filter = "Metin Dosyası |*.txt";
+            file.RestoreDirectory = true;
+            file.Title = "dosya seçiniz";
+
+            if (file.ShowDialog() == DialogResult.OK)
+            {
+                dosyaAdi = file.SafeFileName;
+                dosyaYolu = file.FileName;
+
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            script_olustur();
+            button2.Visible = false;
+            button3.Visible = true;
+            radioButton1.Visible = true;
+            radioButton2.Visible = true;
+            button4.Visible = true;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            AdresGroupCreate();
+            radioButton1.Visible = true;
+            radioButton2.Visible = true;
+            button4.Visible = true;
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            kaydet();
+        }
+    }
+}
